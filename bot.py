860ec9e47699352da8023ec95bc4a769b10702e6
Jonathan Sablotny <jsablotny@gmail.com>
2022-02-04 22:23:33 +0100

    added !help text

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 f = open('list.json')
 answ = json.load(f)
 
+
 #loads config
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -19,34 +20,34 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
 #coin flip kek
-@bot.command(name='flip')
+@bot.command(name='flip', help="just do it\n do a flip")
 async def coin_flip(ctx):
     response = random.choice(answ["flip"])
     await ctx.send(response)
 
 #iterates all mentioned guides
-@bot.command(name='guide')
+@bot.command(name='guide', help="Query all possible guides als follows:\n wenn ich proggn könnte würde hier jetzt was sinnvolles stehen")
 async def flow_chart(ctx, *args):
     for arg in args:
         await ctx.send(answ["guide"][arg])
 
 #iterates all classes (just one tree yet)
-@bot.command(name='class')
+@bot.command(name='class', help="Currently only German Guide")
 async def all_classes(ctx, *args):
     for arg in args:
         await ctx.send(answ["class"][arg])
 
 #iterates all stuff for cosmetics
-@bot.command(name='weeb')
+@bot.command(name='weeb', help="use 'skins' for all released skins on KR client")
 async def weeb_shit_4_leo(ctx, *args):
     for arg in args:
         await ctx.send(answ["weeb"][arg])
 
 #guide for setting up todo list for daily and weekly
-@bot.command(name='todo')
+@bot.command(name='todo', help="usefull todo list for daily and weekly shit")
 async def todo(ctx):
     await ctx.send(answ["todo"]["link"])
-    await ctx.send("Parse follwoing string into " + answ["todo"]["import"] + " code -> paste")
+    await ctx.send("Parse following string into " + answ["todo"]["import"] + " code -> paste")
     await ctx.send(file=discord.File('files/todo.txt'))
 
 #server info
